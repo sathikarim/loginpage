6f8d0c322e6e41725adc340576da72aa76c482d6
kmm <fike2098@gmail.com>
2018-08-16 15:23:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/login/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
